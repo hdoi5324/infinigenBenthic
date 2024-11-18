2255a69b6387cd2751a96fd6ff939c846e83dc13
Heather Doig <hdoi5324@uni.sydney.edu.au>
2024-11-19 09:56:14 +1100

    updated configs

--- scripts/benthic/generate_images.sh
+++ scripts/benthic/generate_images.sh
@@ -3,7 +3,7 @@
 
 overwrite="--overwrite" #--overwrite
 cleanfiles="" #"--cleanup big_files"
-outputfolder="nudi_handfish_rov_v3"
+outputfolder="nudi_handfish_rov_v4"
 num_scenes=100
 pipeline_overrides="" # "--pipeline_overrides manage_datagen_jobs.num_concurrent=2"
 
